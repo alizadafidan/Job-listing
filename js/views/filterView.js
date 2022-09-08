9752ff0e1aca877230d1cd699f9b72019ca3d3f0
Fidan Alizada <alizadafidann@gmail.com>
2022-09-08 14:04:50 +0400

    add filterView

---
+++ js/views/filterView.js
@@ -0,0 +1,39 @@
+class FilterView {
+  _data;
+  _parentElement = document.querySelector(".main");
+  _tabletsContainer = document.querySelector(".filter-tablets");
+  _clearBtn = document.querySelector(".filter-span-clear");
+
+  addHandlerClear(handler) {
+    this._clearBtn.addEventListener("click", () => {
+      this._tabletsContainer.innerHTML = "";
+      handler();
+    });
+  }
+
+  addHandlerRemoveFilter(handler) {
+    this._tabletsContainer.addEventListener("click", (e) => {
+      e.preventDefault();
+      const btn = e.target.closest(".tablet-btn.filtered");
+      if (!btn) return;
+      const removeTag = btn.innerText;
+    });
+  }
+
+  render(data) {
+    this._data = data;
+    const markup = `${data.map((job) => this.jobMarkup(job)).join("")}`;
+    this._clear();
+    this._tabletsContainer.insertAdjacentElement("beforeend", markup);
+  }
+
+  jobMarkup(filterString) {
+    return `<button class="tablet-btn tablet-btn-filtered">${filterString}</button>`;
+  }
+
+  _clear() {
+    this._tabletsContainer.innerHTML = "";
+  }
+}
+
+export default new FilterView();
